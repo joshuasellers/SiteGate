890d38b706e8e0f64d5b5c8fd9904b420992c0da
Josh Sellers <joshuasellersd@gmail.com>
2019-11-10 17:01:54 -0500

    added functionality to add websites

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let blacklist = [];
+let blacklist = ["youtube.com","facebook.com"];
 
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab)
@@ -8,4 +8,18 @@ chrome.tabs.onUpdated.addListener(
         if(changeInfo.url && changeInfo.url.includes('youtube.com')){
             console.log("BLACKLIST");
         }
+    });
+
+chrome.runtime.onMessage.addListener(
+    function(request,sender,sendResponse) {
+
+        if(request.type === "new site") {
+            const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+            let url = request.site.toString();
+            const found = regex.test(url);
+            if (!blacklist.includes(url) && found){
+                blacklist.push(url);
+            }
+        }
+
     });
