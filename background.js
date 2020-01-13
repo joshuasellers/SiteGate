9a50f869f4c4256b119af3bf8728a9fd56da0979
Josh Sellers <joshuasellersd@gmail.com>
2020-01-13 12:36:27 -0500

    changing to confirm

--- background.js
+++ background.js
@@ -5,11 +5,10 @@ let blacklist = ["youtube.com","facebook.com","espn.com"];
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab)
     {
-
         if(changeInfo.url ){
             const blacklisted = blacklist.some(el => changeInfo.url.includes(el));
             if (blacklisted) {
-                alert("BY CLICKING OK YOU AGREE TO WASTE TIME ON THIS SITE")
+                confirm("BY CLICKING OK YOU AGREE TO WASTE TIME ON THIS SITE")
 
             }
         }
@@ -17,7 +16,6 @@ chrome.tabs.onUpdated.addListener(
 
 chrome.runtime.onMessage.addListener(
     function(request,sender,sendResponse) {
-
         if(request.type === "new site") {
             const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
             let url = request.site.toString();
